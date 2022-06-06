51b6a7072dca8c62719627cb8e018d82b25b3808
Unzor <71789103+Unzor@users.noreply.github.com>
2022-06-06 18:38:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const { wakeDyno, wakeDynos } = require('heroku-keep-awake');
 const express = require('express');
 const fs = require("fs");
+var parser = require("cli2json");
+var args = parser.parse(process.argv.slice(2).join(" "), {
+  readCommandAfter: ["-u", "--use", "-a", "--attach"]
+})
 
 const app = express();
 app.use(express.static(__dirname));
 
+var funcs = [];
+
+
+function arrayFindIncludes(r, n) {
+    var u;
+    var a = null;
+    n.forEach(function(n) {
+        if (n.includes(r)) {
+            u = n;
+            a = n
+        } else {
+            u = a
+        }
+    });
+    return u
+}
+
 function FUSE_FN(fn){
+	funcs.push(fn.name);
 	var fne;
 	app.get("/fuse/run/" + fn.name, async (req, res) => {
 		if (req.query.args) {
@@ -24,6 +46,7 @@ function FUSE_FN(fn){
 		}
 	})
   }
+
 
 function rangethrough(sequence, str) {
     var a2 = [];
@@ -47,15 +70,22 @@ function rangethrough(sequence, str) {
     return a3;
 }
 
-if (fs.existsSync("preload.json")) {
-  var preload = JSON.parse(fs.readFileSync("preload.json").toString());
+if (arrayFindIncludes("-u", args.flags) || arrayFindIncludes("--use", args.flags)) {
+  var preload = JSON.parse(fs.readFileSync(args.flags[0].split(" ")[1]).toString());
   preload.files.forEach(function(file){
     eval(fs.readFileSync(file).toString())
   })
+} else if (arrayFindIncludes("-a", args.flags) || arrayFindIncludes("--attach", args.flags)) {
+	    eval(fs.readFileSync(args.flags[0].split(" ")[1]).toString())
 }
+
 app.get('/fuse/attach/:code', (req, res) => {
   eval(atob(req.params.code))
   res.send("attached")
+});
+
+app.get('/fuse/functions', (req, res) => {
+  res.send({list: funcs});
 });
 
 app.listen(process.env.PORT || 3000, () => {
